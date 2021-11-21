ee71fd7c96e9dfe12ecddd6d18fde40b7004e4d5
Pritish Gopaul <pritish.gopaul25@hotmail.com>
2021-11-21 20:32:16 +0400

    Checkout & Google Authentication

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmAp2uExkWX8cy1sXV-lXvYDg77kTqT80",
+    authDomain: "clone-ab944.firebaseapp.com",
+    projectId: "clone-ab944",
+    storageBucket: "clone-ab944.appspot.com",
+    messagingSenderId: "449521899208",
+    appId: "1:449521899208:web:d692548a325d57142377fd"
+  }; 
