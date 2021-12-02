7bf56ad05a34db9709bcfb261fb3c4fd162b4f67
Pritish Gopaul <pritish.gopaul25@hotmail.com>
2021-12-02 19:19:06 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
         domains: ["fakestoreapi.com"]
     },
     env: {
-        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+        stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
     }
 };
